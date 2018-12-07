5aa02f242ace36594e2901a97ae5727a2f9a2ac1
donovanky <donovanky@my.easternct.edu>
2018-12-07 14:03:38 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ ReactDOM.render(
      <App />
    </BrowserRouter>
    , document.getElementById('root'));
- registerserviceWorker();
+ registerServiceWorker();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker.register();
